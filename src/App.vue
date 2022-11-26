5330641306b301c215ccb2f38179bf1b935b5b51
ludoguenet <ludovicguenet@gmx.com>
2022-11-26 14:01:13 +0100

    Made component reusable.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1 class="text-3xl font-bold underline text-emerald-300">
-    Hello world!
-  </h1>
+  <div class="mx-auto container">
+    <select-options
+        :options="people"
+
+        v-model="userId"
+      />
+  </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import SelectOptions from "./components/SelectOptions.vue";
+import {ref} from 'vue';
 
+const userId = ref<number>(1);
+
+const people = [
+  { id: 1, name: 'Wade Cooper' },
+  { id: 2, name: 'Arlene Mccoy' },
+  { id: 3, name: 'Devon Webb' },
+  { id: 4, name: 'Tom Cook' },
+  { id: 5, name: 'Tanya Fox' },
+  { id: 6, name: 'Hellen Schmidt' },
+]
 </script>
---
+++ src/components/SelectOptions.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="fixed top-16 w-72">
+    <Listbox
+        v-on:update:modelValue="value => emit('update:modelValue', value)"
+        v-model="props.modelValue"
+    >
+      <div class="relative mt-1">
+        <ListboxButton
+            class="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+        >
+          <span class="block truncate">{{ username }}</span>
+          <span
+              class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+          >
+            <ChevronUpDownIcon
+                class="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+            />
+          </span>
+        </ListboxButton>
+
+        <transition
+            leave-active-class="transition duration-100 ease-in"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+        >
+          <ListboxOptions
+              class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+          >
+            <ListboxOption
+                v-slot="{ active, selected }"
+                v-for="option in props.options"
+                :key="option.id"
+                :value="option.id"
+                as="template"
+            >
+              <li
+                  :class="[
+                  active ? 'bg-amber-100 text-amber-900' : 'text-gray-900',
+                  'relative cursor-default select-none py-2 pl-10 pr-4',
+                ]"
+              >
+                <span
+                    :class="[
+                    selected ? 'font-medium' : 'font-normal',
+                    'block truncate',
+                  ]"
+                >{{ option.name }}</span
+                >
+                <span
+                    v-if="selected"
+                    class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                >
+                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                </span>
+              </li>
+            </ListboxOption>
+          </ListboxOptions>
+        </transition>
+      </div>
+    </Listbox>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import {
+  Listbox,
+  ListboxLabel,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
+
+const props = defineProps({
+  options: Array,
+  modelValue: {
+    type: Number,
+    default: 1,
+    required: true,
+  }
+});
+
+const username = computed(() => {
+  return props.options.find(option => option.id === props.modelValue)?.name;
+});
+
+const emit = defineEmits(['update:modelValue']);
+</script>
